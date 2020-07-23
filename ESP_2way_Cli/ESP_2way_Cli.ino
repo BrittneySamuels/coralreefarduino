79d2dc0ae901e94e7548647685b52dd9dbd0a7bf
BrittneySamuels <brittney.samuels_1ja@hotmail.com>
2020-07-23 00:22:46 -0500

    first arduino code

---
+++ ESP_2way_Cli/ESP_2way_Cli.ino
@@ -0,0 +1,326 @@
+#include <esp_now.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include <HTTPClient.h>
+#include <Wire.h>
+
+
+// REPLACE WITH THE MAC Address of your receiver 
+uint8_t hubAddress[] = {0x24, 0x6F, 0x28, 0xB0, 0x82, 0x54};
+
+// Define variables to store sensor readings to be sent
+float temperature;
+float water;
+float pH;
+float turbidity;
+
+// Define variables to store incoming user measures
+float incomingTemp;
+float incomingWater;
+float incomingPH;
+float incomingTurb;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    float temp;
+    float wat;
+    float ph;
+    float turb;
+    float Heater;
+    float Inpump;
+    float Outpump;
+} struct_message;
+
+
+// Create a struct_message called BME280Readings to hold sensor readings
+struct_message sendReadings;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message userMeasures;
+
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&userMeasures, incomingData, sizeof(userMeasures));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incomingTemp = userMeasures.temp;
+  incomingWater = userMeasures.wat;
+  incomingPH = userMeasures.ph;
+  incomingTurb = userMeasures.turb;
+  Serial.println ("Recieved Temperature:");
+  Serial.println (incomingTemp);
+  Serial.println ("Recieved Water Level:");
+  Serial.println (incomingWater);
+  Serial.println ("Recieved pH:");
+  Serial.println (incomingPH);
+  Serial.println ("Recieved TDS:");
+  Serial.println (incomingTurb);
+}
+
+
+ //GLOBAL DEFINITIONS AND PORT IDENTIFICATIONS
+    //Lights and Sound
+    #define heater 23// port for Increase Temp 
+    #define decLight 13 // port for Decrease light(BLUE)
+    #define warnLight 19 //port for Warning Light (RED)
+    #define buzzer 18// port for the warning sound (BUZZER)
+    
+  //Water Level Sensor
+    #define waterPower 26  //Applies Power to sensor
+    #define waterSensor 39 //Collects analog reading
+    int waterVal = 0; //Initialize Water Level value storage
+    int highWaterCount = 0; //water counter for warnings (HIGH)
+    int lowWaterCount = 0; //water counter for warnings (LOW)
+
+    //temperature
+    const int oneWireBus = 5;
+    OneWire oneWire(oneWireBus);
+    DallasTemperature sensors(&oneWire); 
+    int highTempCount = 0; //Temp counter for warnings (HIGH)
+    int lowTempCount = 0; //Temp counter for warnings (LOW)
+    
+
+
+    //Water Pumps
+    #define iFPower 14 //Powers the IN Flow water pump
+    #define oFPower 27 //Powers the OUT Flow water pump
+    int waterMax = 900; //highest water level
+    int waterMin = 500; //lowest water level
+
+//pH sensor
+    #define SensorPin 34          // the pH meter Analog output is connected with the Arduino’s Analog
+    unsigned long int avgValue;  //Store the average value of the sensor feedback
+    float b;
+    int buf[10],temp;
+
+//TDS Sensor
+    #define sensorPin 2
+ 
+    int sensorValue = 0;
+    float tdsValue = 0;
+    float Voltage = 0;
+    float tds = 0;
+    float TDSCon = 0.000998859;
+    float TDS = 0; 
+    float factor = 33.85;
+    
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  //initialize pH module
+ // phModule.begin();
+   
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_AP_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Init ESP-NOW
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, hubAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+   else 
+   {
+    Serial.println("Peer Added Successfully!");
+   }
+   
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+
+//Define OUTPUTS
+pinMode (heater, OUTPUT);
+pinMode (decLight, OUTPUT);
+pinMode (warnLight, OUTPUT);
+pinMode (buzzer, OUTPUT);
+pinMode (waterPower, OUTPUT);
+pinMode (iFPower, OUTPUT);
+pinMode (oFPower, OUTPUT);
+pinMode (13,OUTPUT); 
+
+Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+}
+
+
+ 
+void loop() {
+
+    float Heater = 1000;
+    float Inpump = 1000;
+    float Outpump = 1000;
+    float Blank = 1000;
+
+   // Confirm Connection
+    if (esp_now_init() == ESP_OK) 
+    {
+    Serial.println(" ESP-NOW Connected");
+    }
+    if (esp_now_init() != ESP_OK) 
+    {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+    }
+  
+// Recieve Temp
+        int tempMax = incomingTemp+1; //hotest allowable temperature
+        int tempMin = incomingTemp-1; //coldest allowable temperature
+        sensors.requestTemperatures(); 
+        float temperatureC = sensors.getTempCByIndex(0);
+        Serial.print(temperatureC);
+        Serial.println("ºC");
+        temperature = temperatureC;
+        Serial.print ("Temperature is:");
+        Serial.println (temperature);
+
+
+  //Compare TEMPERATURE and execute ACTIONS
+       //High Temperature
+            if (temperature > tempMax)
+            {
+              digitalWrite(heater, LOW);  //Ensure the Temperature Increaser is OFF
+              digitalWrite(decLight, HIGH);  // Turn the Temperture Decreaser ON
+              Serial.println("Water cooler ON");
+                     
+            }
+        
+       //Low Temperature
+           if (temperature < tempMin)
+           {
+              digitalWrite(decLight, LOW);  //Ensure the Temperature Decreaser is OFF
+              digitalWrite(heater, HIGH);  // Turn the Temperture Increaser ON
+              Serial.println("Water Heater is ON");
+              Heater = 2500;
+           }
+
+//WATER LEVEL  
+          water = analogRead(waterSensor);    // Read the analog value form sensor
+          Serial.print ("Water Level is:");
+          Serial.println (water);
+          
+ 
+   //Compare WATER and execute ACTIONS
+        //High Water
+            if (water > waterMax) 
+            {
+             digitalWrite(iFPower, LOW);  //Ensure the Inflow pump is OFF
+              digitalWrite(oFPower, HIGH);  // Turn the Outflow pump ON
+              Serial.println("OUTflow Water Pump is ON");
+              Serial.println("INflow Water Pump is OFF"); 
+              Outpump = 3500;
+            }
+   
+        //Low Water
+            if (water < waterMin) 
+            {
+             digitalWrite(oFPower, LOW);  //Ensure the Outflow pump is OFF
+              digitalWrite(iFPower, HIGH);  // Turn the Inflow pump ON
+              Serial.println("INflow Water Pump is ON"); 
+              Serial.println("OUTflow Water Pump is OFF");
+              Inpump = 4500;        
+            }
+
+ //pH Reading
+     for(int i=0;i<10;i++)       //Get 10 sample value from the sensor for smooth the value
+      { 
+        buf[i]=analogRead(SensorPin);
+        delay(10);
+      }
+      for(int i=0;i<9;i++)        //sort the analog from small to large
+      {
+        for(int j=i+1;j<10;j++)
+        {
+          if(buf[i]>buf[j])
+          {
+            temp=buf[i];
+            buf[i]=buf[j];
+            buf[j]=temp;
+          }
+        }
+      }
+      avgValue=0;
+      for(int i=2;i<8;i++)                      //take the average value of 6 center sample
+        avgValue+=buf[i];
+      float phValue=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
+      phValue=3.5*phValue;
+
+
+//TDS Readings
+
+    sensorValue = analogRead(sensorPin);
+    Voltage = sensorValue*5/1024.0; //Convert analog reading to Voltage
+    tds =(133.42*Voltage*Voltage*Voltage - 255.86*Voltage*Voltage + 857.39*Voltage)*0.5; //Convert voltage value to TDS value
+    TDS= tds * TDSCon;
+    tdsValue = factor + TDS;
+    //tdsValue = tds * TDSCon;
+    Serial.println("TDS Value:");
+    Serial.println(TDS);
+
+//package readings to send
+              
+  // Set values to send
+  Serial.println("Sending the Readings");
+  sendReadings.temp = temperature;
+  sendReadings.wat = water;
+  sendReadings.ph = phValue;
+  sendReadings.turb = tdsValue;
+  sendReadings.Heater = Heater;
+  sendReadings.Inpump = Inpump;
+  sendReadings.Outpump = Outpump;
+  
+ // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(hubAddress, (uint8_t *) &sendReadings, sizeof(sendReadings));
+
+  if (result == ESP_OK) 
+    {
+      Serial.println("Reading Sent with success");
+    }
+  else 
+    {
+      Serial.println("Error sending the Readings");
+    }
+
+     delay(5000);
+}
+
+
+
+   
+      
+   
+    
---
+++ ESP_2way_Serv/ESP_2way_Serv.ino
@@ -0,0 +1,482 @@
+#include <ArduinoJson.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Wire.h>
+
+#define buzzer 18// port for the warning sound (BUZZER)
+
+//#include <string>
+int tank_id = 1;
+
+// ACCESS POINT credentials
+//const char* ssidAP = "ESP32-Access-Point";
+//const char* passwordAP = "12345678";
+// Wi-Fi channel for the access point (must match the sender channel)
+#define CHAN_AP 1
+
+// REPLACE WITH THE MAC Address of your receiver 
+  uint8_t tempAddress[] = {0x24, 0x6f, 0x28, 0xB0, 0x92, 0x80}; //Temperature Board
+
+//Wifi and server setup
+  const char* ssid = "Corals";
+  const char* password = "coralconnect";
+  char* sensorData = "http://192.168.43.225:8081/sensordata";
+  char* activityLog = "http://192.168.43.225:8081/createActivityLog";
+  char* GetRequest = "http://192.168.43.225:8081/configurationsGet/?tankid=1";  
+
+
+// Define variables to store user readings to be sent
+  float temperature;
+  float water;
+  float pH;
+  float turbidity;
+
+// Define variables to store incoming readings
+  float incomingTemp;
+  float incomingWater;
+  float incomingPH;
+  float incomingTurb;
+  float incomingHeater;
+  float incomingInpump;
+  float incomingOutpump;
+
+
+// Define variables to store incoming Activities Log in TEXT
+
+    String Heaterlog;
+    String Inpumplog;
+    String Outpumplog;
+    
+// apiKeyValue value must match the PHP file /post-data.php 
+  String data_string = "This data is being sent to the DB:";
+
+//dummy vatlues
+  float incomingSalinity = 34;
+  float Blank = 0;
+   
+// Variable to store if sending data was successful
+  String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    float temp;
+    float wat;
+    float ph;
+    float turb;
+    float Heater;
+    float Inpump;
+    float Outpump;
+} struct_message;
+
+// Create a struct_message to hold user measures that will be sent to the boards
+struct_message userMeasures;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+
+// Callback when data is sent
+  void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) 
+    {
+    Serial.print("\r\nLast Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+      if (status ==0){
+        success = "Delivery Success :)";
+      }
+      else{
+        success = "Delivery Fail :(";
+      }
+    }
+
+//warning checks
+  int tempWarn = 0;
+  int watWarn = 0;
+  int phWarn = 0;
+  int turbWarn = 0;
+
+
+// Callback when data is received
+  void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) 
+    {
+    memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+    Serial.print("Bytes received: ");
+    Serial.println(len);
+  
+      //Values coming from the child board
+      incomingTemp = incomingReadings.temp;
+      incomingWater = incomingReadings.wat;
+      incomingPH = incomingReadings.ph;
+      incomingTurb = incomingReadings.turb;
+      incomingHeater = incomingReadings.Heater;
+      incomingInpump = incomingReadings.Inpump;
+      incomingOutpump = incomingReadings.Outpump;
+      
+      //converting activities to text 
+        //Heater
+            if (incomingHeater>1100)
+              {
+                Heaterlog = "Low Temp, Water Heater turned ON";
+              }
+              else
+              {
+                Heaterlog = "Water Heater is OFF";
+              }
+      
+        //Inflow Pump
+            if (incomingInpump>1100)
+              {
+                Inpumplog = "Low Water Level, Inflow Water Pump turned ON";
+              }
+              else
+              {
+                Inpumplog = "Inflow Water Pump is OFF";
+              }
+      
+        //Outflow Pump
+            if (incomingOutpump>1100)
+              {
+                Outpumplog = "High Water Level,Outflow Water Pump turned ON";
+              }
+              else
+              {
+                Outpumplog = "Outflow Water Pump is OFF";
+              }
+
+        
+      Serial.println ("Recieved Temperature:");
+      Serial.println (incomingTemp);
+      Serial.println ("Recieved Water Level:");
+      Serial.println (incomingWater);
+      Serial.println ("Recieved pH:");
+      Serial.println (incomingPH);
+      Serial.println ("Recieved TDS:");
+      Serial.println (incomingTurb);
+      Serial.println ("The Heater Code is:");
+      Serial.println (incomingHeater);
+      Serial.println ("The INpump Code is:");
+      Serial.println (incomingInpump);
+      Serial.println ("The OUTpump Code is:");
+      Serial.println (incomingOutpump);
+    }
+   
+void setup() 
+{
+  pinMode (buzzer, OUTPUT);
+  
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  Serial.println("Ready");    //Test the serial monitor
+  
+  WiFi.begin(ssid, password);
+
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("Connected to network named: ");
+  Serial.print(ssid);
+     
+  // Set device as a Wi-Fi Station  
+  WiFi.mode(WIFI_AP_STA);
+
+   // Set device as an access point
+//  WiFi.softAP(ssidAP, passwordAP, CHAN_AP, true); 
+
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK)
+    {
+      Serial.println("Error initializing ESP-NOW");
+      return;
+    }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, tempAddress, 6);
+  peerInfo.channel = 1;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    {
+      Serial.println("Failed to add peer");
+      return;
+    }
+    else 
+    {
+      Serial.println("Peer Added Successfully!");
+    }
+  
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() 
+{
+
+    //Check WiFi connection status
+ if(WiFi.status()== WL_CONNECTED)
+  {
+    HTTPClient http;   //declare http client
+
+    //Domain name with URL path or IP address with path
+    http.begin(sensorData); //start the route for the sensor data
+  
+    //packaging readings to be sent to the DB
+    String httpRequestData =   "data_string=" + data_string + "&temperature=" + (incomingTemp) + "&TDS=" + (incomingTurb)+ "&salinity=" + (incomingSalinity)+ "&ph=" + (incomingPH) + "&waterlevel=" + (incomingWater) + "&tank_id=" + (tank_id);
+
+    // Specification of content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+  
+     // Send HTTP POST request
+      int httpResponseCode = http.POST(httpRequestData);
+
+          if (httpResponseCode>0) 
+              {
+              Serial.print("HTTP Response code: ");
+              Serial.println(httpResponseCode);
+              Serial.println(http.getString());
+              }
+              else 
+              {
+                Serial.print("Error code: ");
+                Serial.println(http.errorToString(httpResponseCode));
+              }
+    // Free resources
+    http.end();
+  
+
+ //Sending Activities log
+    //Domain name with URL path or IP address with path
+    HTTPClient httpActivity;   //declare http client
+    http.begin(activityLog);
+      
+    //packaging readings to be sent to the DB
+    String httpRequestHeater =   "activity=" + (Heaterlog) + "&tank_id=" + (tank_id);
+    String httpRequestInpump =   "activity=" + (Inpumplog) + "&tank_id=" + (tank_id);
+    String httpRequestOutpump =  "activity=" + (Outpumplog) + "&tank_id=" + (tank_id);
+    
+     // Specification of content-type header
+     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+      Serial.println("httpRequests for Activities ");
+      Serial.println(httpRequestHeater);
+      Serial.println(httpRequestInpump);
+      Serial.println(httpRequestOutpump);
+      
+       // Send HTTP POST request
+        int httpResponseCodeOutpump = http.POST(httpRequestOutpump);
+        delay(750);
+
+         // Specification of content-type header
+     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        int httpResponseCodeHeater = http.POST(httpRequestHeater);
+        delay(750);
+
+         // Specification of content-type header
+     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        int httpResponseCodeInpump = http.POST(httpRequestInpump);
+        
+    
+      if (httpResponseCodeHeater>0) 
+        {
+          Serial.print("HTTP HEATER Response code: ");
+          Serial.println(httpResponseCodeHeater);
+          Serial.println(http.getString());
+        }
+        else 
+          {
+            Serial.print("HEATER Error code: ");
+            Serial.println(http.errorToString(httpResponseCodeHeater));
+          }
+
+      if (httpResponseCodeInpump>0) 
+        {
+          Serial.print("HTTP INPUMP Response code: ");
+          Serial.println(httpResponseCodeInpump);
+          Serial.println(http.getString());
+        }
+        else 
+          {
+            Serial.print("INPUMP Error code: ");
+            Serial.println(http.errorToString(httpResponseCodeInpump));
+          }
+
+      if (httpResponseCodeOutpump>0) 
+        {
+          Serial.print("HTTP OUTPUMP Response code: ");
+          Serial.println(httpResponseCodeOutpump);
+          Serial.println(http.getString());
+        }
+        else 
+          {
+            Serial.print("INPUMP Error code: ");
+            Serial.println(http.errorToString(httpResponseCodeOutpump));
+          }
+        
+        // Free resources
+        httpActivity.end();
+  
+  //USER CONFIG REQUEST HANDLER //
+      HTTPClient httpGet;   //declare http client
+
+    //Domain name with URL path or IP address with path
+      httpGet.begin(GetRequest);
+
+      httpGet.addHeader("Content-Type", "application/json","charset=utf-8");   
+
+      String tankID = String("{\"tankid\":") + (tank_id) +  String("}"); 
+
+      int GetResponseCode =  httpGet.POST(tankID);  // Make the request
+      
+      String ConfigValues = httpGet.getString();
+      
+      //JSON Parser//
+      StaticJsonDocument<250> doc;
+      DeserializationError err = deserializeJson(doc, ConfigValues);
+            
+      if (err)
+        {
+          Serial.print("ERROR: ");
+          Serial.print(err.c_str());
+          return;
+       }
+      //END JSON Parser//
+
+       temperature = doc["temperature"];
+        water = doc["waterlevel"];
+        pH = doc["ph"];
+        turbidity= doc["TDS"];
+    
+       Serial.print("User Temperature is: ");
+       Serial.println(temperature); 
+       Serial.print("User Water Level is: ");
+       Serial.println(water);
+       Serial.print("User pH is: ");
+       Serial.println(pH); 
+       Serial.print("User TDS is: ");
+       Serial.println(turbidity); 
+
+       
+      if (GetResponseCode > 0)
+      {        
+        Serial.print("HTTP GET Response code: ");
+        Serial.println(GetResponseCode);
+        Serial.println(httpGet.getString());
+        Serial.println(ConfigValues);
+      }
+     else 
+      {
+       Serial.print("Error code: ");
+       Serial.println(httpGet.errorToString(GetResponseCode));
+      }
+
+        // Free resources
+      httpGet.end();
+  
+  }
+  
+ if(WiFi.status()!= WL_CONNECTED) 
+   {
+     Serial.println("WiFi Disconnected");
+   }
+ 
+ //Trigger warnings
+      if (incomingTemp != temperature)
+        {
+          tempWarn++;
+          Serial.println(tempWarn);
+        }
+        if (tempWarn == 4)
+          {
+            warning();
+            tempWarn=0;
+          }
+    
+      if (incomingWater != water)
+        {
+          watWarn++;
+          Serial.println(watWarn);
+        }
+          if (watWarn == 4)
+          {
+            warning();
+            watWarn=0;
+          }
+    
+      if (incomingPH != pH)
+        {
+          phWarn++;
+          Serial.println(phWarn);
+        }
+          if (phWarn == 4)
+          {
+            warning();
+            phWarn=0;
+          }
+    
+      if (incomingTurb != turbidity)
+        {
+          turbWarn++;
+          Serial.println(turbWarn);
+        }
+          if (turbWarn == 4)
+          {
+            warning();
+            turbWarn=0;
+          }
+        
+ 
+  // Set values to send
+      userMeasures.temp = temperature;
+      userMeasures.wat = water;
+      userMeasures.ph = pH;
+      userMeasures.turb = turbidity;
+      userMeasures.Heater = Blank; 
+      userMeasures.Inpump = Blank;
+      userMeasures.Outpump = Blank;
+  
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(tempAddress, (uint8_t *) &userMeasures, sizeof(userMeasures));
+   
+  if (result == ESP_OK) 
+    {
+      Serial.println("User configuration Sent with success");
+    }
+  else
+    {
+      Serial.println("Error sending the User configuration");
+    }
+   delay(15000);
+}
+ 
+//Warning
+    int warning()
+      {
+      for (int w = 0; w < 4; w++) // sound horn 4 times
+        {
+        Serial.println ("WARNING!!!");
+        digitalWrite (buzzer, HIGH);
+        delay(1500);
+        digitalWrite (buzzer, LOW);
+        delay(750);  
+        }
+      }
+    
+  
+  
